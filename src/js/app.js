5d08fb8dd027c219f1610c470c87808709c12034
YanPetrov7 <yanemerald2004@gmail.com>
2023-05-25 02:02:41 +0300

    add express-session

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,20 @@
 const http = require('http');
 const express = require('express');
-const app = express();
+const session = require('express-session');
 
+const app = express();
 const port = 3000;
 const server = http.createServer(app);
+
+app.use(
+  session({
+    secret: 'secretkey',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
