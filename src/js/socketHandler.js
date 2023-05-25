13802614da4d3f0c7f51c8876f1168d9766fb6fa
YanPetrov7 <yanemerald2004@gmail.com>
2023-05-26 01:12:03 +0300

    create a func for defining tables for new dialogs

--- src/js/socketHandler.js
+++ src/js/socketHandler.js
@@ -1,4 +1,33 @@
-const {User} = require('../../models');
+const {User} = require('../models');
+const {DataTypes} = require('sequelize');
+const db = require('../models');
+
+const defineTable = (tableName) => {
+  return db.sequelize.define(tableName, {
+    sender: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    receiver: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    message: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: db.Sequelize.literal('CURRENT_TIMESTAMP'),
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: db.Sequelize.literal('CURRENT_TIMESTAMP'),
+      allowNull: false,
+    },
+  });
+};
 
 // Function to handle WebSocket connection
 const handleSocketConnection = async (ws) => {
