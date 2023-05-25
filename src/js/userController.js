7fa43ca71431467929f80bd542f37e1cade195de
YanPetrov7 <yanemerald2004@gmail.com>
2023-05-25 13:59:33 +0300

    add redirection to home if user logged in from different controllers

--- src/js/userController.js
+++ src/js/userController.js
@@ -5,34 +5,50 @@ const { logMessage } = require('./func.js');
 const logType = 'CONTROLLER';
 
 exports.getWelcomePage = (req, res) => {
+  if (req.session.loggedin) {
+    const username = req.session.username;
+    return res.redirect(`/home?user=${username}`);
+  }
   const filePath = path.join(__dirname, '../pages/welcome.html');
   return res.sendFile(filePath);
 };
 
 exports.getLoginPage = (req, res) => {
+  if (req.session.loggedin) {
+    const username = req.session.username;
+    return res.redirect(`/home?user=${username}`);
+  }
   const filePath = path.join(__dirname, '../pages/login.html');
   return res.sendFile(filePath);
 };
 
 exports.getRegisterPage = (req, res) => {
+  if (req.session.loggedin) {
+    const username = req.session.username;
+    return res.redirect(`/home?user=${username}`);
+  }
   const filePath = path.join(__dirname, '../pages/register.html');
   return res.sendFile(filePath);
 };
 
 exports.getHomePage = (req, res) => {
-  const filePath = path.join(__dirname, '../pages/home.html');
-  return res.sendFile(filePath);
+  if (req.session.loggedin) {
+    const filePath = path.join(__dirname, '../pages/home.html');
+    return res.sendFile(filePath);
+  } else {
+    return res.redirect('/');
+  }
 };
 
 exports.registerUser = async (req, res) => {
   const { username, password } = req.body;
-  if (!username || !password) {
+  if (!username || !password) { // If username or password is missing
     return res.send('Please enter both Username and Password!');
   }
 
   try {
-    const existingUser = await User.findOne({ where: { username } });
-    if (existingUser) {
+    const existingUser = await User.findOne({ where: { username } }); // Checking if user already exists
+    if (existingUser) { // If user already exists
       const errorMessage = `User with username: '${username}' already exists`;
       logMessage(logType, errorMessage, 'error'); // Logging error, user already exists
       return res.send(errorMessage);
@@ -43,9 +59,9 @@ exports.registerUser = async (req, res) => {
     req.session.username = username;
     await User.create({ username, password: hash });
 
-    const infoMessage = `Created new user with name ${username}`;
+    const infoMessage = `Created user with name ${username}`;
     logMessage(logType, infoMessage, 'success'); // Logging succsessful login message
-    // Redirect to future page
+    return res.redirect(`/home?user=${username}`);
   } catch (err) {
     const errorMessage = `Error occurred while registering user: ${err.message}`;
     logMessage(logType, errorMessage, 'error'); // Logging error while registering user
