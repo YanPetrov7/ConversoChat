40fc04fa87ff6a6a3d17d92850aba63ae85b8302
YanPetrov7 <yanemerald2004@gmail.com>
2023-05-25 03:39:06 +0300

    create login.html, register.html & update router, controller for new pages

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const userController = require('./userController.js');
 
 router.get('/', userController.getWelcomePage);
+router.get('/login', userController.getLoginPage);
+router.get('/register', userController.getRegisterPage);
 
 module.exports = router;
--- src/js/userController.js
+++ src/js/userController.js
@@ -4,3 +4,13 @@ exports.getWelcomePage = (req, res) => {
   const filePath = path.join(__dirname, '../pages/welcome.html');
   return res.sendFile(filePath);
 };
+
+exports.getLoginPage = (req, res) => {
+  const filePath = path.join(__dirname, '../pages/login.html');
+  return res.sendFile(filePath);
+};
+
+exports.getRegisterPage = (req, res) => {
+  const filePath = path.join(__dirname, '../pages/register.html');
+  return res.sendFile(filePath);
+};
