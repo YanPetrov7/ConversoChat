ed37db44a3c661e8409272d46753b8d8bb54ae5b
YanPetrov7 <yanemerald2004@gmail.com>
2023-05-25 21:58:38 +0300

    create a test version of user contacts

--- src/js/socketHandler.js
+++ src/js/socketHandler.js
@@ -1,6 +1,10 @@
+const {User} = require('../../models');
+
 // Function to handle WebSocket connection
 const handleSocketConnection = async (ws) => {
-  
+  const users = await User.findAll();
+  const usernames = users.map((item) => item.username);
+  ws.send(JSON.stringify(usernames)); // Sending receivers to the WebSocket client
 }
 
 module.exports = handleSocketConnection;
