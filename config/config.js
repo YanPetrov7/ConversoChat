d81b4999a7b4beb25933d634efbd77c22b6d7836
YanPetrov7 <yanemerald2004@gmail.com>
2023-06-06 01:44:54 +0300

    make data passwords inaccessable for the public

--- config/config.js
+++ config/config.js
@@ -1,16 +1,18 @@
-{
+/* eslint-disable */
+
+module.export = {
   "development": {
     "username": "root",
-    "password": "test1234",
+    "password": process.env.DB_PASSWORD,
     "database": "converso_chat",
     "host": "mysql_db",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": "test1234",
+    "password": process.env.TEST_DB_PASSWORD,
     "database": "converso_chat_test",
     "host": "mysql_db_test",
     "dialect": "mysql"
   }
-}
+};
