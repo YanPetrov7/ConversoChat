f0279c07800b74a13b2ee43de8e31d0eca266d50
YanPetrov7 <yanemerald2004@gmail.com>
2023-06-07 01:11:47 +0300

    update docker-compose usage of secret keys

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
-{
+/* eslint-disable */
+require('dotenv').config();
+
+module.exports = {
   "development": {
     "username": "root",
-    "password": "test1234",
+    "password": process.env.DB_PASSWORD,
     "database": "converso_chat",
     "host": "mysql_db",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": "test1234",
+    "password": process.env.TEST_DB_PASSWORD,
     "database": "converso_chat_test",
     "host": "mysql_db_test",
     "dialect": "mysql"
   }
-}
+};
